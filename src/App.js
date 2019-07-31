998c80211f6580c1cdfd6b1f5955d7fe89138814
Natasha Henry <natashaphenry@yahoo.com>
2019-07-30 20:18:20 -0400

    added the Home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Home from './components/Home';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <Home />
     </div>
   );
 }
